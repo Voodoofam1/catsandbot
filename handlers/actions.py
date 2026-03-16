7a08e728bd1d579a0832fb63ac43b86f4d8d8636
Voodoofam1 <tkacivan83@gmail.com>
2026-03-16 13:39:40 +0300

    реакция на кнопки обнять и погладить кота

---
+++ handlers/actions.py
@@ -0,0 +1,21 @@
+import os
+
+history = []
+
+def handle_actions(bot, message):
+    '''выполнение действие при нажатии на кнопку пользователем: погладить и обнять кота '''
+    text = message.text
+
+    if "погладить кота😘" in text:
+        history.append("погладить кота😘")
+        photo_path = os.path.join("media", "cat1.gif")
+        if os.path.exists(photo_path):
+            with open(photo_path, "rb") as f:
+                bot.send_animation(message.chat.id, f, caption="кот доволен")
+
+    elif "обнять кота😂" in text:
+        history.append("обнять кота😂")
+        photo_path = os.path.join("media", "cat2.gif")
+        if os.path.exists(photo_path):
+            with open(photo_path, "rb") as f:
+                bot.send_animation(message.chat.id, f, caption="кот счастлив")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from handlers.start import start
+from handlers.actions import handle_actions
 import telebot
 
 load_dotenv()
@@ -11,6 +12,10 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     start(bot, message)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    handle_actions(bot, message)
 
 if __name__ == '__main__':
     print("бот работает")
